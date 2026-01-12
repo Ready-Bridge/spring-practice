c30e9d8d5e07efc4431608d9932a6d131b1496a0
한준교 <hanjungyo5533@gmail.com>
2026-01-13 00:49:57 +0900

    test(todo): update TodoControllerTests to reflect v2 and DTO changes

--- todo-list/src/test/java/com/asdf/todo/controller/TodoControllerTests.java
+++ todo-list/src/test/java/com/asdf/todo/controller/TodoControllerTests.java
@@ -8,7 +8,8 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import com.asdf.todo.entity.Todo;
+import com.asdf.todo.dto.TodoRequestDto;
+import com.asdf.todo.dto.TodoResponseDto;
 import com.asdf.todo.service.TodoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -32,12 +33,11 @@ class TodoControllerTests {
     @Test
     @DisplayName("ID로 할 일을 조회하면 200 OK와 해당 데이터를 반환한다")
     void testGetTodoById() throws Exception {
-        Todo todo = new Todo();
-        todo.setId(1L);
-        todo.setTitle("Test Todo");
+        TodoResponseDto todo = new TodoResponseDto(1L, "Test Todo", "Description", false);
+
         given(todoService.findById(1L)).willReturn(todo);
 
-        mockMvc.perform(get("/api/todos/v1/1")
+        mockMvc.perform(get("/api/todos/v2/1") // v2 반영
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1L))
@@ -49,17 +49,17 @@ class TodoControllerTests {
     void testGetAllTodos_Empty() throws Exception {
         given(todoService.findAll()).willReturn(Collections.emptyList());
 
-        mockMvc.perform(get("/api/todos/v1"))
+        mockMvc.perform(get("/api/todos/v2")) // v2 반영
                 .andExpect(status().isNoContent());
     }
 
     @Test
     @DisplayName("할 일 목록이 존재할 때 200 OK와 리스트를 반환한다")
     void testGetAllTodos_Success() throws Exception {
-        Todo todo = new Todo(1L, "Test Todo", "Description", false);
+        TodoResponseDto todo = new TodoResponseDto(1L, "Test Todo", "Description", false);
         given(todoService.findAll()).willReturn(Collections.singletonList(todo));
 
-        mockMvc.perform(get("/api/todos/v1")
+        mockMvc.perform(get("/api/todos/v2") // v2 반영
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").value(1L))
@@ -69,60 +69,55 @@ class TodoControllerTests {
     @Test
     @DisplayName("새로운 할 일을 생성하면 201 Created와 생성된 데이터를 반환한다")
     void testCreateTodo() throws Exception {
-        Todo requestTodo = new Todo();
-        requestTodo.setTitle("New Todo");
+        // RequestDto 사용
+        TodoRequestDto requestDto = new TodoRequestDto("New Todo", "Description");
+        // ResponseDto 사용
+        TodoResponseDto responseDto = new TodoResponseDto(1L, "New Todo", "Description", false);
 
-        Todo savedTodo = new Todo();
-        savedTodo.setId(1L);
-        savedTodo.setTitle("New Todo");
+        given(todoService.save(any(TodoRequestDto.class))).willReturn(responseDto);
 
-        given(todoService.save(any(Todo.class))).willReturn(savedTodo);
-
-        mockMvc.perform(post("/api/todos/v1")
+        mockMvc.perform(post("/api/todos/v2") // v2 반영
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(requestTodo))
+                        .content(objectMapper.writeValueAsString(requestDto))
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(1L))
                 .andExpect(jsonPath("$.title").value("New Todo"));
 
-        verify(todoService).save(any(Todo.class));
+        verify(todoService).save(any(TodoRequestDto.class));
     }
 
     @Test
     @DisplayName("할 일을 수정하면 200 OK와 수정된 데이터를 반환한다")
     void testUpdateTodo() throws Exception {
-        Todo requestTodo = new Todo();
-        requestTodo.setTitle("Updated Todo");
+        // RequestDto 및 ResponseDto 사용
+        TodoRequestDto requestDto = new TodoRequestDto("Updated Todo", "Description");
+        TodoResponseDto responseDto = new TodoResponseDto(1L, "Updated Todo", "Description", false);
 
-        Todo updatedTodo = new Todo();
-        updatedTodo.setId(1L);
-        updatedTodo.setTitle("Updated Todo");
-
-        given(todoService.findById(1L)).willReturn(new Todo());
-        given(todoService.save(any(Todo.class))).willReturn(updatedTodo);
+        // 수정 시나리오: findById로 존재 확인 후 update 호출
+        given(todoService.findById(1L)).willReturn(responseDto);
+        given(todoService.update(anyLong(), any(TodoRequestDto.class))).willReturn(responseDto);
 
         mockMvc.perform(
-                        put("/api/todos/v1/1")
+                        put("/api/todos/v2/1") // v2 반영
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(requestTodo))
+                                .content(objectMapper.writeValueAsString(requestDto))
                                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1L))
                 .andExpect(jsonPath("$.title").value("Updated Todo"));
 
         verify(todoService).findById(1L);
-        verify(todoService).save(any(Todo.class));
+        verify(todoService).update(anyLong(), any(TodoRequestDto.class));
     }
 
     @Test
     @DisplayName("존재하는 할 일을 삭제하면 204 No Content를 반환하고 서비스를 호출한다")
     void testDeleteTodo_Success() throws Exception {
-        Todo todo = new Todo();
-        todo.setId(1L);
-        given(todoService.findById(1L)).willReturn(todo);
+        TodoResponseDto responseDto = new TodoResponseDto(1L, "Title", "Desc", false);
+        given(todoService.findById(1L)).willReturn(responseDto);
 
-        mockMvc.perform(delete("/api/todos/v1/1"))
+        mockMvc.perform(delete("/api/todos/v2/1")) // v2 반영
                 .andExpect(status().isNoContent());
 
         verify(todoService).delete(1L);
@@ -133,10 +128,10 @@ class TodoControllerTests {
     void testDeleteTodo_NotFound() throws Exception {
         given(todoService.findById(1L)).willReturn(null);
 
-        mockMvc.perform(delete("/api/todos/v1/1"))
+        mockMvc.perform(delete("/api/todos/v2/1")) // v2 반영
                 .andExpect(status().isNotFound());
 
         verify(todoService).findById(1L);
-        verify(todoService, never()).delete(anyLong());
+        verify(verify(todoService, never())).delete(anyLong());
     }
 }
